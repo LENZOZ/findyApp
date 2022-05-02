8131a3a574c6c61c5cee78017c8c8a0fdbfcaffe
LENZOZ <lorenzo.baeza.n@gmail.com>
2022-05-02 19:20:00 -0400

    Se cambia etiqueta p

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Que pasa con el ssdmarceliño
+         al fin ctm
         </p>
         <a
           className="App-link"
