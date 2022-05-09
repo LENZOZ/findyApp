6af63ea65306a958ee3a096c9e27772255d182b0
LENZOZ <lorenzo.baeza.n@gmail.com>
2022-05-09 14:32:26 -0400

    navbar defectuosa css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { NavBar } from './components/navbar/NavBar';
+
+//Importar componentes
+
+export function App(){
+return(
+  <div>
+      <NavBar/>
+  </div>
+);
+
+}
